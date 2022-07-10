ef5a748dc44fdf5387c790617314b2e2f69eb170
mosharif30 <sharifkhani.mr@gmail.com>
2022-07-10 19:05:12 +0430

    display posts in main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,27 +3,28 @@ import { PostCard, PostWidget, Categories } from "../components";
 import { getPosts } from "../services/index";
 
 const Home = ({ posts }) => {
-  console.log("sala,", posts);
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-900">
+    <>
       <Head>
         <title>Cms Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-12 gap-12  text-white p-8  ">
-        <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
-          ))}
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8">
-            <PostWidget />
-            <Categories />
+      <div className="container mx-auto px-10 mb-8 ">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className=" lg:col-span-8 col-span-1">
+            {posts.map((post) => (
+              <PostCard post={post.node} key={post.title} />
+            ))}
+          </div>
+          <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Home;
