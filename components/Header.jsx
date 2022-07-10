fef1c79fd74e43cdfa0fe27701a5979dc205e56b
mosharif30 <sharifkhani.mr@gmail.com>
2022-07-10 16:45:37 +0430

    code set up & create some components

---
+++ components/Header.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { useContext } from "react";
+
+const categories = [
+  { name: "React", slug: "react" },
+  { name: "Web", slug: "web" },
+];
+const Header = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8 ">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              Graph CMS
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+        <div className="hidden md:float-left md:contents">
+          <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+            Resume
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/PostWidget.jsx
@@ -0,0 +1,5 @@
+const PostWidget = () => {
+  return <>PostWidget</>;
+};
+
+export default PostWidget;
