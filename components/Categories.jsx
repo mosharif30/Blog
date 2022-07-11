c40f80d4e907a6684d17232b19c0f04adf688e5e
mosharif30 <sharifkhani.mr@gmail.com>
2022-07-11 16:09:52 +0430

    change widget design

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -13,7 +13,7 @@ const Categories = () => {
   }, []);
 
   return (
-    <div className="text-white shadow-lg rounded-lg p-8 pb-4 mb-4 ">
+    <div className="text-white shadow-lg rounded-lg p-8 pb-4 mb-4 bg-gray-900">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -21,7 +21,7 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug]);
 
   return (
-    <div className="text-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+    <div className="text-white shadow-lg rounded-lg p-8 pb-12 mb-8 bg-gray-900">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         {slug ? "Related Posts" : "Recent Posts"}
       </h3>
