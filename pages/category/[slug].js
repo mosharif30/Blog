a3f5616e339acd669cfb3ccc5fcbe8dcd3d1d579
mosharif30 <sharifkhani.mr@gmail.com>
2022-07-11 14:15:20 +0430

    add reverse

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -15,7 +15,7 @@ const CategoryPost = ({ posts }) => {
     <div className="container mx-auto px-1 mb-2 ">
       <div className="grid  grid-cols-1 lg:grid-cols-12 gap-3">
         <div className="grid  lg:grid-cols-2 lg:col-span-8 col-span-1 gap-6">
-          {posts.map((post, index) => (
+          {posts.reverse().map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>
