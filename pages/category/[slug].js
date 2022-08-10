61d2dbdf5cbd51d5eb90f3a76a0d65344769b538
mosharif30 <sharifkhani.mr@gmail.com>
2022-08-10 14:16:35 +0430

    category slug bug fix

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -37,7 +37,7 @@ const CategoryPost = ({ posts }) => {
 export default CategoryPost;
 
 // Fetch data at build time
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const posts = await getCategoryPost(params.slug);
 
   return {
